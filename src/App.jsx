d788eb0ecca8206d608e8ea498fada10b9c6f9bc
Asad Khan <mrasad10khan@gmail.com>
2024-03-02 16:59:53 +0500

    Adding skills

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./Sections/Navbar";
 import Hero from "./Sections/Hero";
+import Skills from "./Sections/Skills";
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Skills />
     </>
   );
 }
