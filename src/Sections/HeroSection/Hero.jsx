2eb3f046e3984374818ae1e2c92297031cb95486
Asad Khan <mrasad10khan@gmail.com>
2024-03-30 17:11:43 +0500

    Minor Adjustments

--- src/Sections/HeroSection/Hero.jsx
+++ src/Sections/HeroSection/Hero.jsx
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css";
 import picture from "../../images/ASAD4.png";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 
 function Hero() {
+  const [name, setName] = useState("ASAD");
+  const [secName, setSecName] = useState("KHAN");
+  const [profession, setProfession] = useState("FRONT-END");
+  const [secProfession, setSecProfession] = useState("DEVELOPER");
+
   return (
     <>
       <section className="hero-container">
         <img className="hero-img" src={picture} />
         <h1 className="hero-name">
-          ASAD <br></br> <span className="hero-s-name"> KHAN </span>
+          {name} <br></br> <span className="hero-s-name"> {secName} </span>
         </h1>
 
         <h1 className="profession-name">
-          FRONT-END <br></br>{" "}
-          <span className="profession-s-name"> DEVELOPER </span>
+          {profession} <br></br>{" "}
+          <span className="profession-s-name"> {secProfession} </span>
         </h1>
       </section>
     </>
