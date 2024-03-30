4780ee742fe274ad0f943ed872bd27f510e2b680
Asad Khan <mrasad10khan@gmail.com>
2024-03-30 22:53:45 +0500

    Minor Adjustments

--- src/Sections/HeroSection/Hero.jsx
+++ src/Sections/HeroSection/Hero.jsx
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
 import "./Hero.css";
 import picture from "../../images/ASAD4.png";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
+import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 
 function Hero() {
-  const [name, setName] = useState("ASAD");
-  const [secName, setSecName] = useState("KHAN");
-  const [profession, setProfession] = useState("FRONT-END");
-  const [secProfession, setSecProfession] = useState("DEVELOPER");
+  const [name, setName] = useState("Asad Khan");
+
+  const [profession, setProfession] = useState("Front End Developer");
 
   return (
     <>
       <section className="hero-container">
         <img className="hero-img" src={picture} />
-        <h1 className="hero-name">
-          {name} <br></br> <span className="hero-s-name"> {secName} </span>
-        </h1>
 
-        <h1 className="profession-name">
-          {profession} <br></br>{" "}
-          <span className="profession-s-name"> {secProfession} </span>
+        <h1 className="hero-name">
+          <Typewriter
+            words={[`My name is ${name}`, `I am a ${profession}`]}
+            deleteSpeed={70}
+            typeSpeed={70}
+            delaySpeed={1500}
+            loop={true}
+            cursor
+            cursorStyle=">"
+            cursorColor="#ffff"
+          />
         </h1>
       </section>
     </>
