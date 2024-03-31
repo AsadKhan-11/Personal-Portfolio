72418757d67625780afcb8d50c2e177585721ffb
Asad Khan <mrasad10khan@gmail.com>
2024-03-31 12:02:59 +0500

    Minor Adjustment

--- vite.config.js
+++ vite.config.js
@@ -6,4 +6,9 @@ import envCompatible from "vite-plugin-env-compatible";
 export default defineConfig({
   envPrefix: "REACT_APP",
   plugins: [envCompatible(), react()],
+  build: {
+    rollupOptions: {
+      external: ["react-simple-typewriter"],
+    },
+  },
 });
