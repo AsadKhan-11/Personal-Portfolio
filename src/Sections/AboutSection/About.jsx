6c3cf9fa7d0ee03f040c8398fd44c8eb6c652e7f
Asad Khan <mrasad10khan@gmail.com>
2024-04-06 23:45:01 +0500

    Modifying About me page

--- src/Sections/AboutSection/About.jsx
+++ src/Sections/AboutSection/About.jsx
@@ -10,16 +10,34 @@ function About() {
             ABOUT ME
             <span className="about-sec-header">WHO AM I? </span>
           </h1>
-          <p className="about-txt">
-            Hello there! I'm Asad Ahmed Khan, a front end web developer based in
-            Lahore, Pakistan. With a passion for web development, I specialize
-            in developing user-friendly digital experiences. I'm dedicated to
-            delivering high-quality web development services. As a freelance web
-            developer, I offer personalized solutions modified to your needs. My
-            expertise extend to HTML, CSS, JavaScript, React, and WordPress,
-            ensuring that your project is in capable hands. Need a Shopify web
-            developer? Look no further—I've got you covered.
-          </p>
+
+          <div className="about-tags">
+            <div className="about-tags-box">
+              <h1 className="about-info">10+</h1>
+              <p className="about-desc">Happy clients</p>
+            </div>
+            <div className="about-tags-box">
+              <h1 className="about-info">1+</h1>
+              <p className="about-desc">Years of experience</p>
+            </div>
+            <div className="about-tags-box">
+              <h1 className="about-info">20+</h1>
+              <p className="about-desc">Projects done</p>
+            </div>
+          </div>
+
+          <div className="about-txt-container">
+            <p className="about-txt">
+              Hello there! I'm Asad Ahmed Khan, a front end web developer based
+              in Lahore, Pakistan. With a passion for web development, I
+              specialize in developing user-friendly digital experiences. I'm
+              dedicated to delivering high-quality web development services. As
+              a freelance web developer, I offer personalized solutions modified
+              to your needs. My expertise extend to HTML, CSS, JavaScript,
+              React, and WordPress, ensuring that your project is in capable
+              hands.
+            </p>
+          </div>
         </div>
       </section>
     </>
