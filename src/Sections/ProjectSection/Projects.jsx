3a13473f0b7d96ddd83da69db125b9b76ac1c8ec
Asad Khan <mrasad10khan@gmail.com>
2024-05-01 15:55:35 +0500

    Adding descriptions to my projects

--- src/Sections/ProjectSection/Projects.jsx
+++ src/Sections/ProjectSection/Projects.jsx
@@ -8,35 +8,54 @@ import omnifood from "../../projects/omnifood.png";
 import weather from "../../projects/weather.png";
 
 const data = [
-  { name: "News App", img: `${news}` },
-  { name: "Weather Forecaster", img: `${weather}` },
+  {
+    name: "News App",
+    img: `${news}`,
+    content:
+      "My news app built with React is a comprehensive platform with the latest headlines and stories from around the world. The app provides easy access to news articles across various categories such as business, technology, sports, entertainment, and more. Powered by a robust news API, the app dynamically fetches and displays real-time news content, complete with headlines, summaries, and images.",
+  },
+  {
+    name: "Weather Forecaster",
+    img: `${weather}`,
+    content:
+      "My weather forecasting app is a simple yet effective tool designed to provide up-to-date weather information at your fingertips. Users can enter the name of a city in the search bar, and the app fetches real-time weather data using a weather API. The weather information includes temperature, humidity, wind speed, and a description of the weather conditions.",
+  },
   {
     name: "Youtube Clone",
     img: `${youtube}`,
+    content:
+      "My YouTube Clone built with React looks and feels similar to the real YouTube website. It's designed to show a collection of video thumbnails and titles in a neat grid layout. Users can browse through these thumbnails to discover different videos, just like on YouTube. Although this clone doesn't actually play videos, it provides a visual preview of what the videos look like.",
   },
+
   {
     name: "Music Player",
     img: `${player}`,
   },
-  { name: "Calculator", img: `${calculator}` },
-  { name: "Omnifood", img: `${omnifood}` },
+  {
+    name: "Calculator",
+    img: `${calculator}`,
+    content:
+      "I created a calculator app using React. This calculator allows users to perform basic arithmetic operations like addition, subtraction, multiplication, and division. It features a clean and interactive interface with buttons for numbers and operators. Using React's component-based architecture, I implemented logic to handle user inputs and update the display based on the calculations. ",
+  },
+  {
+    name: "Omnifood",
+    img: `${omnifood}`,
+    content:
+      "Omnifood is a cool website that makes ordering food super easy. It's built using HTML and CSS, which are tools for creating websites. The website lets you order delicious and healthy meals from local restaurants without any hassle. You can see yummy pictures of different dishes and explore various cuisines. Omnifood is all about making food delivery simple and enjoyable.",
+  },
 ];
 
 function Projects() {
   const [index, setIndex] = useState(0);
 
   let nextImg = () => {
-    setIndex((index) => {
-      if (index == data.length - 1) return 0;
-      return index + 1;
-    });
+    const num = (index + 1) % data.length;
+    setIndex(num);
   };
 
   let prevImg = () => {
-    setIndex((index) => {
-      if (index === 0) return data.length - 1;
-      return index - 1;
-    });
+    const num = (index - 1 + data.length) % data.length;
+    setIndex(num);
   };
 
   return (
@@ -61,7 +80,10 @@ function Projects() {
                 key={num}
                 className={num === index ? " slide slide-active" : "slide"}
               >
-                <img src={d.img} className="project-img " />
+                <div className="project-desc">
+                  <img src={d.img} className="project-img " />
+                  <p className="project-content">{d.content} </p>
+                </div>
                 <h3 className="project-name">{d.name}</h3>
               </div>
             ))}
